1824c3d83ebb09f16a4ff9207caaf969892587eb
telkomdev-odiapratama <odiapratama@gmail.com>
2021-02-05 00:31:38 +0700

    Animate bottom menu

--- app/src/main/java/com/problemsolver/androidplayground/ui/MainActivity.kt
+++ app/src/main/java/com/problemsolver/androidplayground/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.navigation.fragment.NavHostFragment
 import com.problemsolver.androidplayground.R
 import com.problemsolver.androidplayground.base.activity.BaseActivity
 import com.problemsolver.androidplayground.databinding.ActivityMainBinding
+import com.problemsolver.androidplayground.utils.hideView
+import com.problemsolver.androidplayground.utils.showView
 import com.problemsolver.androidplayground.utils.toGone
 import com.problemsolver.androidplayground.utils.toVisible
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,15 +16,25 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun setLayout() = R.layout.activity_main
 
-    override fun viewOnReady() = Unit
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menu?.let {
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_main_menu) as NavHostFragment
             menuInflater.inflate(R.menu.bottom_menu, menu)
             binding.bnvMain.setupWithNavController(menu, navHostFragment.navController)
         }
         return false
+    }
+
+    override fun viewOnReady() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_main_menu) as NavHostFragment
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.home, R.id.explore, R.id.more -> binding.bnvMain.showView()
+                else -> binding.bnvMain.hideView()
+            }
+        }
     }
 
     fun showLoading(show: Boolean) {
--- app/src/main/java/com/problemsolver/androidplayground/utils/ViewExt.kt
+++ app/src/main/java/com/problemsolver/androidplayground/utils/ViewExt.kt
@@ -1,7 +1,10 @@
 package com.problemsolver.androidplayground.utils
 
 import android.view.View
+import android.view.animation.TranslateAnimation
 import android.widget.ImageView
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import coil.api.load
 import coil.transform.CircleCropTransformation
 
@@ -18,4 +21,20 @@ fun ImageView.loadCircleImage(url: String) {
         crossfade(true)
         transformations(CircleCropTransformation())
     }
+}
+
+fun View.showView(duration: Long = 400) {
+    if (isVisible) return
+    toVisible()
+    val animate = TranslateAnimation(0f, 0f, height.toFloat(), 0f)
+    animate.duration = duration
+    startAnimation(animate)
+}
+
+fun View.hideView(duration: Long = 400) {
+    if (isGone) return
+    toGone()
+    val animate = TranslateAnimation(0f, 0f, 0f, height.toFloat())
+    animate.duration = duration
+    startAnimation(animate)
 }
