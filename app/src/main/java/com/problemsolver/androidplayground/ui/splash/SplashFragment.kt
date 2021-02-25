426135472e9435f2a40e4ccf3071770ac04744ab
telkomdev-odiapratama <odiapratama@gmail.com>
2021-02-25 14:48:11 +0700

    Add splash screen

---
+++ app/src/main/java/com/problemsolver/androidplayground/ui/splash/SplashFragment.kt
@@ -0,0 +1,26 @@
+package com.problemsolver.androidplayground.ui.splash
+
+import androidx.navigation.findNavController
+import com.problemsolver.androidplayground.R
+import com.problemsolver.androidplayground.base.fragment.BaseFragment
+import com.problemsolver.androidplayground.databinding.FragmentSplashBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>() {
+
+    private val navController by lazy { requireView().findNavController() }
+
+    override fun setLayout() = R.layout.fragment_splash
+
+    override fun viewOnReady() {
+        GlobalScope.launch {
+            delay(3000)
+            navController.navigate(
+                SplashFragmentDirections.splashFragmentToMenuFragment()
+            )
+        }
+    }
+
+}
